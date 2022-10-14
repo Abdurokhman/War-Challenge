d9d07729ce9b8b3b1155343a49173e0416e1d115
Abdurokhman Mukhanbetov <112958052+Abdurokhman@users.noreply.github.com>
2022-10-14 15:18:20 +0400

    Add files via upload

---
+++ war-challenge/war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Abdurokhman Mukhanbetov on 26.07.22.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
